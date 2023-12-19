cd27cc7aebc5951e9d3826eb6bce6dfb21c7970b
nojood2023 <151244570+nojood2023@users.noreply.github.com>
2023-12-19 22:56:05 +0300

    Add files via upload

---
+++ autoLayout/autoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  autoLayout
+//
+//  Created by Nojood Aljuaid  on 03/06/1445 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
